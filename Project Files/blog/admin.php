0ee38e3dfa1656a4bd339398f183af5331d3709f
MikeJLeon <michaeljleon@live.com>
2018-04-22 22:26:03 -0700

    Revert "Version 1.2"
    This reverts commit 694fcffda3be28cb5e46ef10a967678d2144dd11.

--- Project Files/blog/admin.php
+++ Project Files/blog/admin.php
@@ -2,28 +2,34 @@
 <?php
 	session_start();
 	include "conn.php";
-	if(isset($_SESSION["action"])){
-		switch($_SESSION["action"]){
-		}
+	/* I need to change this I think */
+	$mysqli = mysqli_connect('localhost', 'root', '', 'articles');
+	if ($mysqli->connect_error) {
+		die('Error : ('. $mysqli->connect_error.') '. $mysqli->connect_error);
 	}else{
+		if(isset($_SESSION["action"])){
+			switch($_SESSION["action"]){
+			}
+		}else{
 ?>
-		<html>
-			<head>
-				<title>Admin</title>
-			</head>
-			<body>
-				<form action="add.php" method="post">
-					<h1>Welcome admin</h1>
-					<h3>title</h3>
-					<input type="text" name="title">
-					<h3>summary</h3>
-					<input type="text" name="summary">
-					<h3>content</h3>
-					<textarea rows="4" cols="50" name="content"></textarea>
-					<input type="submit" name="submit">
-				</form>
-			</body>
-		</html>
+			<html>
+				<head>
+					<title>Admin</title>
+				</head>
+				<body>
+					<form action="add.php" method="post">
+						<h1>Welcome admin</h1>
+						<h3>title</h3>
+						<input type="text" name="title">
+						<h3>summary</h3>
+						<input type="text" name="summary">
+						<h3>content</h3>
+						<textarea rows="4" cols="50" name="content"></textarea>
+						<input type="submit" name="submit">
+					</form>
+				</body>
+			</html>
 <?php
+		}
 	}
 ?>
